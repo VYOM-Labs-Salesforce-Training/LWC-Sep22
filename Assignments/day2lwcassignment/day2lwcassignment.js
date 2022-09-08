4cd8a4997a6fdd89e68e092bdb9f9c549d5460d9
prajkulkarni11 <oukulkarni1290@gmail.com>
2022-09-08 17:52:32 +0530

    lwcday1assignment_prajakta

---
+++ Assignments/day2lwcassignment/day2lwcassignment.js
@@ -0,0 +1,52 @@
+import { LightningElement, track } from 'lwc';
+ 
+export default class CandidateForm extends LightningElement {
+ 
+   @track firstName = '';
+   @track lastName = '';
+   @track dateofbirth='';
+   @track isMale = false;
+   @track isFemale = false;
+   @track ageAbove60=false;
+ 
+   handleChange(event) {
+       const field = event.target.name;
+       if (field === 'firstName') {
+           this.firstName = event.target.value;
+       } else if (field === 'lastName') {
+           this.lastName = event.target.value;
+       }
+   }
+ 
+   get uppercasedFullName() {
+       return `${this.firstName} ${this.lastName}`.toUpperCase();
+   }
+ 
+   dateofbirth='';  
+        age='';    
+   handleonchange(event){
+      
+       this.dateofbirth=event.target.value.split('-')[0];
+      
+      console.log('handleonchange method '+this.dateofbirth);
+       this.age=(new Date().getFullYear() - this.dateofbirth);
+ 
+       if(this.age>=60){
+           this.ageAbove60=true;
+       }if(this.age<=60){
+           this.ageAbove60=false;
+       }
+       if(this.age>18){
+           this.ageBelow18=true;
+       }
+       }
+ 
+       handleChangeForMale(event){
+               this.isMale=true;
+               this.isFemale=False;
+       }
+       handleChangeForFemale(event){
+               this.isFemale=true;
+               this.isMale=False;
+       }
+}
---
+++ Assignments/lwcfirstassignment/lwcfirstassignment.js
@@ -0,0 +1,11 @@
+/**
+ * @ Author: Prajakta kulkarni
+ * @ Create Time: 2022-09-05 18:39:26
+ * @ Modified by: Your name
+ * @ Modified time: 2022-09-07 17:39:29
+ * @ Description:Vyom labs’ is setting up an app which can display sales information for its Sales executive. 
+ */
+
+import { LightningElement } from 'lwc';
+
+export default class Lwcfirstassignment extends LightningElement {}
